23b7c9e60c4161454c33f86719ce8b62411ec866
= <twlmed212@gmail.com>
2023-10-05 17:56:32 -0700

    updatedé

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - main entry point
+ * Return:  returns zero meanig success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	/*
+	 * return return zero meaning sucess
+	 */
+	return (0);
+}
